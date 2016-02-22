8a12eb8bc1f9bbaaf69429889426a17b487b3f1e
dulei <duleigiser@icloud.com>
2016-02-22 11:02:53 +0800

    Update collect.js

--- collect.js
+++ collect.js
@@ -1,1 +1,11 @@
-//空
+//字符串反转
+function reverse(str){
+  return str.split('').reverse().join('')
+}
+function reverse1(str){
+  var res='';
+  for(var i = 0; i<str.length;i++){
+      res+=str[str.length-1-i];
+  }
+  return res;
+}
