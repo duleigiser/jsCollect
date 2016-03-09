4dd5554f9dc2f3d6712fbe4949e78b42699c4b9a
dulei <duleigiser@icloud.com>
2016-03-09 11:16:20 +0800

    提取url中的参数

--- collect.js
+++ collect.js
@@ -9,3 +9,17 @@ function reverse1(str){
   }
   return res;
 }
+//提取url字符串到json数据；
+function queryString(url){
+  var reg_urlc = /^[^\?]+\?([\w\W]+)$/,
+      reg_para = /([^&=]+)=([\w\W]*?)(&|$|#)/g,
+      arr_url = reg_url.exec(url),
+      res = {};
+      if(arr_url && arr_url[1]) {
+        var str_para= arr_url[1],result;
+        while((result = reg_para.exec(str_para)) != null){
+          res[result[1]] = result[2];
+        }
+      }
+  return res;     
+}
