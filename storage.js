e3054f99a2a6905b14af65374c89b2a06e660188
dulei <duleigiser@icloud.com>
2016-07-04 10:42:49 +0800

    storage 本地存储封装

--- storage.js
+++ storage.js
@@ -1,1 +1,30 @@
 
+export default {
+    local: {
+        get(key){
+            let strValue = localStorage.getItem(key);
+            return JSON.parse(strValue);
+        },
+        set(key, jsonValue){
+            var strValue = JSON.stringify(jsonValue);
+            localStorage.setItem(key, strValue)
+        },
+        remove(key){
+            localStorage.removeItem(key);
+        }
+    },
+    session: {
+        get(key){
+            let strValue = sessionStorage.getItem(key);
+            return JSON.parse(strValue);
+        },
+        set(key, jsonValue){
+            var strValue = JSON.stringify(jsonValue);
+            sessionStorage.setItem(key, strValue)
+        },
+        remove(key){
+            sessionStorage.removeItem(key);
+        }
+    }
+
+}
